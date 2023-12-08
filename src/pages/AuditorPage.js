10c897a79fb7fa3c50984d3d12fae43de6556682
Varun Khachane <varunkhachane31@gmail.com>
2023-12-09 03:40:06 +0530

    Update

--- src/pages/AuditorPage.js
+++ src/pages/AuditorPage.js
@@ -1,3 +1,14 @@
+import backgroundImage from '../assets/img/audit-bg.svg'
 export default function AuditorPage() {
-    return ( <div></div>);
+    return (<div className="h-screen relative">
+        <img
+            src={backgroundImage}
+            alt="Background"
+            className="h-full w-full object-cover absolute inset-0 z-0"
+        />
+        
+        <div className=''>
+
+        </div>
+    </div>);
 }
--- src/pages/EstimatorPage.js
+++ src/pages/EstimatorPage.js
@@ -1,3 +1,14 @@
+import backgroundImage from '../assets/img/estimate-bg.svg'
 export default function EstimatorPage() {
-    return (<div></div>);
+    return (<div className="h-screen relative">
+        <img
+            src={backgroundImage}
+            alt="Background"
+            className="h-full w-full object-cover absolute inset-0 z-0"
+        />
+
+        <div className=''>
+
+        </div>
+    </div>);
 }
