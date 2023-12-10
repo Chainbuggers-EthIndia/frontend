c389195a2d6f76e2bbf20c44877439957c23da0e
Varun Khachane <varunkhachane31@gmail.com>
2023-12-10 07:29:24 +0530

    ui fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
 import logo from '../assets/img/main-logo.svg'
 export default function Footer() {
     return (
-        <div className="h-[20vh] w-full bg-green flex">
-            <img src={logo} />
-            <div className='text-lg text-semibold font-roboto'>LEXECO @ 2023. All rights reserved.</div>
+        <div className="h-[8vh] w-full bg-green flex justify-between">
+            <img src={logo} className='py-2 ml-3'/>
+            <div className='text-lg text-center text-semibold mt-4 px-8 font-roboto'>LEXECO @ 2023. All rights reserved.</div>
         </div>
     );
 }
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,27 +1,22 @@
 import React from 'react';
-import title from '../assets/img/title.svg';
 import NavigationBar from '../components/NavigationBar';
 import decarbon from '../assets/img/decarbon.svg'
-import carboncradit from '../assets/img/carboncradit.svg'
+import carboncredit from '../assets/img/carboncredit.svg'
 import logo from '../assets/img/favicon-logo.svg'
 import findmore from '../assets/img/findmore.svg'
 import Footer from '../components/Footer';
+
 export default function LandingPage() {
     return (
         <div className="h-screen w-full relative">
-            {/* <img
-                src={title}
-                alt="Background"
-                className="h-full w-full object-cover absolute inset-0 z-0"
-            /> */}
             <div className="fixed top-0 left-0 right-0 z-10">
                 <NavigationBar />
             </div>
             <div className="overflow-y-auto overflow-x-hidden">
-                <section className="h-screen bg-gray-200 bg-[url('./assets/img/title.svg')] bg-no-repete bg-cover">
+                <section className="h-screen bg-[url('./assets/img/title.svg')] bg-no-repeat bg-cover">
                     <div className='h-screen flex justify-center items-center'>
                         <div className='flex flex-col items-center justify-around'>
-                            <div className='w-1/4 h-1/4'>
+                            <div className='w-1/2'>
                                 <img src={logo} />
                             </div>
                             <div className='text-7xl text-extrabold text-white mb-2 font-helvetica underline-offset-2'>LEXECO</div>
@@ -37,40 +32,27 @@ export default function LandingPage() {
                         </div>
                     </div>
                 </section>
-                <section className="h-screen bg-gray-200 relative flex flex-row-reverse items-center justify-between">
+                <section className="h-screen bg-darkgreen relative flex flex-row items-center justify-between">
+                    <div className="w-[40%] ml-5 text-center flex flex-col px-10">
+                        <h1 className="text-5xl text-start text-white font-bold">What is Carbon Credits?</h1>
+                        <h1 className="text-xl font-helvetica font-[400] text-offwhite2 text-left">
+                            Carbon credits are permits allowing companies to emit a specific amount of greenhouse gases. One credit equals one ton of emissions. Trading these credits creates incentives for emission reduction and environmental sustainability.
+                        </h1>
+                    </div>
+                    <div className="w-[70%] flex items-end justify-end">
+                        <img src={carboncredit} className='w-2/3 h-2/3 rounded-l-lg right-0' />
+                    </div>
+                </section>
+                <section className="h-screen bg-[url('./assets/img/s3.svg')] bg-no-repeat bg-cover bg-offwhite2 relative flex flex-row-reverse items-center justify-between">
                     <div className="w-[40%] text-center flex flex-col mr-[10vh]">
-                        <h1 className="text-3xl text-start text-[#000000] font-bold">What is Decarbonization ?</h1>
-                        <h1 className="text-xl font-helvetica font-[400] text-[#4B5652] text-left">
-                        Decarbonization slashes CO2, shifts to clean energy, and enhances efficiency. Capturing industry emissions is crucial, with global collaboration driving a sustainable future.
+                        <h1 className="text-5xl text-start text-green font-bold underline-offset-4">What is Decarbonization ?</h1>
+                        <h1 className="text-xl font-helvetica font-[400] text-white text-left">
+                            Decarbonization slashes CO2, shifts to clean energy, and enhances efficiency. Capturing industry emissions is crucial, with global collaboration driving a sustainable future.
                         </h1>
                     </div>
                     <div className="w-[70%]">
                         <img src={decarbon} className='w-2/3 h-2/3 rounded-l-lg' />
                     </div>
-                </section>
-                {/* Section 2 */}
-                <section className="h-screen bg-gray-200 relative flex flex-row items-center justify-between gap-[10vh]">
-                    <div className="w-[40%] text-center flex flex-col px-10">
-                        <h1 className="text-3xl text-start text-[#000000] font-bold">What is Carbon Credits?</h1>
-                        <h1 className="text-xl font-helvetica font-[400] text-[#4B5652] text-left">
-                        Carbon credits are permits allowing companies to emit a specific amount of greenhouse gases. One credit equals one ton of emissions. Trading these credits creates incentives for emission reduction and environmental sustainability.
-                        </h1>
-                    </div>
-                    <div className="w-[70%] flex items-end justify-end">
-                        <img src={carboncradit} className='w-2/3 h-2/3 rounded-l-lg right-0' />
-                    </div>
-                </section>
-
-                {/* Section 3 */}
-                <section className="h-screen bg-gray-400">
-                    <h1 className="text-3xl font-bold text-center pt-8">Section 3</h1>
-                    {/* Add your content for Section 3 here */}
-                </section>
-
-                {/* Section 4 */}
-                <section className="h-screen bg-gray-500">
-                    <h1 className="text-3xl font-bold text-center pt-8">Section 4</h1>
-                    {/* Add your content for Section 4 here */}
                 </section>
             </div>
             <div className="">
